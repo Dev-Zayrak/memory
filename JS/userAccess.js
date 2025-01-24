98503d219395ddc1f181d4e4a5c38b40c60a28cf
Dev-Zayrak <calle.florian@gmail.com>
2025-01-24 05:20:09 +0100

    update nav barre

--- JS/userAccess.js
+++ JS/userAccess.js
@@ -2,6 +2,17 @@ const container = document.getElementById('container')
 const registerBtn = document.getElementById('register')
 const loginBtn = document.getElementById('login')
 
+document.addEventListener("DOMContentLoaded", ()=>{
+    const urlParams = new URLSearchParams(window.location.search);
+    const formType = urlParams.get("form");
+
+    if(formType === "register"){
+        container.classList.add("active")
+    }
+})
+
+
+
 registerBtn.addEventListener('click', ()=>{
     container.classList.add("active")
 })
